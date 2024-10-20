e6b63cbf1433f2a6c1e0fc0fd4eb7080d8e95232
Muz Muz <murtaza.deen135@gmail.com>
2024-10-20 15:58:25 +0100

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # Create test user
-User.create!(email: 'test.user@example.com', name: 'John Doe', password: 'helloworld123')
+user = User.create!(email: 'test.user@example.com', name: 'John Doe', password: 'helloworld123')
 
 # Create sample projects
 
-Project.create!(name: 'Notifications System', description: 'The aim of this project is to send system notifications.')
-Project.create!(name: 'Expense Management System', description: 'Manages expenses of individual persons.')
-Project.create!(name: 'Tic Tac Toe', description: 'A two person game.')
+Project.create!(name: 'Notifications System', description: 'The aim of this project is to send system notifications.', user:)
+Project.create!(name: 'Expense Management System', description: 'Manages expenses of individual persons.', user:)
+Project.create!(name: 'Tic Tac Toe', description: 'A two person game.', user:)
